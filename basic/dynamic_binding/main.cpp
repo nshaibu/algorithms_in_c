696dde10cad2217925fd53be31074ae9ea3edd38
nshaibu <nafiushaibu1@gmail.com>
2021-02-11 11:51:42 +0000

    dynaic casting

--- basic/dynamic_binding/main.cpp
+++ basic/dynamic_binding/main.cpp
@@ -8,8 +8,17 @@ using namespace std;
 int main(void) {
     cout << "Dynamic Binding" << endl;
     Person* pp[2];
-    pp[0] = new Person("1", "nafiu");
+    Student* sp;
+    pp[0] = new Person("nafiu", "1");
     pp[1] = new Student("2", "shaibu", "English", 2020);
+
+    cout << "Dynamic Casting" << endl;
+    for  (int i = 0; i < 2; i++) {
+        sp = dynamic_cast<Student*>(pp[i]);
+        if (sp != nullptr) 
+            sp->changeMajor("CS");
+    }
+
     for (int i = 0; i < 2; i++) {
         pp[i]->print(); // This will resolve in type's 'print' method.
         cout << endl;
